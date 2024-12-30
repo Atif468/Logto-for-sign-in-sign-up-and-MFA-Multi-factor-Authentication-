1e7a0e4fa92b16d2d69cad71d0400ad16fe890ab
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-12-30 15:04:43 +0530

    Add files via upload

--- src/app/logto.tsx
+++ src/app/logto.tsx
@@ -2,7 +2,7 @@ export const logtoConfig = {
     endpoint: 'https://9kg787.logto.app/',
     appId: '9f46tt1t69g3tg55jairc',
     appSecret: 'mi9WaBXlbKNmA6Zu20P0wEppCYMn00R3',
-    baseUrl: 'http://localhost:3000', // Change to your own base URL
+    baseUrl: 'https://logto-test-pi.vercel.app/', // Change to your own base URL
     cookieSecret: 'Jo6Mfo3fTo8qCrJvmMHNrimpwaFWkjQZ', // Auto-generated 32 digit secret
     cookieSecure: process.env.NODE_ENV === 'production',
   };
